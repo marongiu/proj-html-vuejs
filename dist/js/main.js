9fc2a2fc3815e8755727c440d2313b80d1093dcb
Andrea <andreamarongiu05@gmail.com>
2021-04-21 12:36:14 +0200

    Inizio navbar

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,23 @@
 var app = new Vue({
   el: '#root',
   data: {
-
+    navbar: [
+      {
+        name: 'Home'
+      },
+      {
+        name: 'Landing',
+        content: 'fa-angle-down'
+      },
+      {
+        name: 'Pages',
+        content: 'fa-angle-down'
+      },
+      {
+        name: 'Docs',
+        content: 'fa-angle-down'
+      }
+    ]
   },
   mounted() {
     console.log("Ok");
